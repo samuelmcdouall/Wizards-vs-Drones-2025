a7ab7e16f5cda9d4388448ae6aacc55b8f032d4e
samuelmcdouall <samuelmcdouall@gmail.com>
2024-09-10 15:23:47 +0100

    Rotate model

---
+++ Wizards vs Drones/Assets/Scripts/WVDPlayerModelRotate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WVDPlayerModelRotate : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, Input.GetAxis("Mouse X") * 10.0f, 0.0f); // todo put in mouse sensitivity
+    }
+}
