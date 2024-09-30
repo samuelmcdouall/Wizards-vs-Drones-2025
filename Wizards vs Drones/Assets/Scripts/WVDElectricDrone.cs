ddc6480109561adafcd8055c22070fc0ca4e60e1
samuelmcdouall <samuelmcdouall@gmail.com>
2024-09-30 22:57:31 +0100

    Some initial set up for the electric drone, need to figure out its chasing state next

--- Wizards vs Drones/Assets/Scripts/WVDElectricDrone.cs
+++ Wizards vs Drones/Assets/Scripts/WVDElectricDrone.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WVDEnemy : WVDEntity
+public class WVDElectricDrone : WVDEntity
 {
     // Start is called before the first frame update
     public override void Start()
@@ -13,11 +13,15 @@ public class WVDEnemy : WVDEntity
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 
     public enum EnemyState
     {
-
+        Chasing,
+        ChargingUp,
+        Attacking,
+        Decharge // stand still just after attack
     }
 }
