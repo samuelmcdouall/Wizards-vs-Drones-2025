0ef42c7bad768f5fc17b6dfebd3fc8217296159c
samuelmcdouall <samuelmcdouall@gmail.com>
2024-11-24 02:15:39 +0000

    Added lifetime + flashing effect to battery to mark its about to be destroyed

--- Wizards vs Drones/Assets/Scripts/WVDBatteryPickUp.cs
+++ Wizards vs Drones/Assets/Scripts/WVDBatteryPickUp.cs
@@ -7,9 +7,21 @@ public class WVDBatteryPickUp : MonoBehaviour
     [SerializeField]
     int _value;
     Rigidbody _rb;
+    [SerializeField]
+    float _lifeTime;
+    [SerializeField]
+    float _startFlashingThreshold;
+    [SerializeField]
+    float _flashPeriod;
+    float _timer;
+    [SerializeField]
+    GameObject _batteryModel;
+    Coroutine _flashCoroutine;
 
     private void Start()
     {
+        _timer = _lifeTime;
+        Destroy(gameObject, _lifeTime);
         _rb = GetComponent<Rigidbody>();
         float randX = Random.Range(-200.0f, 200.0f);
         float randY = Random.Range(200.0f, 300.0f);
@@ -28,5 +40,25 @@ public class WVDBatteryPickUp : MonoBehaviour
             other.gameObject.transform.parent.gameObject.GetComponent<WVDPlayer>().BatteryCount += _value;
             Destroy(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        if (_timer < _startFlashingThreshold)
+        {
+            if (_flashCoroutine == null)
+            {
+                _flashCoroutine = StartCoroutine(ChangeAfterFlashPeriod());
+            }
+        }
+        _timer -= Time.deltaTime;
+
+    }
+
+    IEnumerator ChangeAfterFlashPeriod()
+    {
+        yield return new WaitForSeconds(_flashPeriod);
+        _batteryModel.SetActive(!_batteryModel.activeSelf);
+        _flashCoroutine = null;
     }
 }
