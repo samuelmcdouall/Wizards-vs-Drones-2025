cbbd884895c44a14bbc6968012d17f6cdd666191
samuelmcdouall <samuelmcdouall@gmail.com>
2025-01-19 01:52:25 +0000

    Update IWVDDamageable.cs

--- Wizards vs Drones/Assets/Scripts/Interfaces/IWVDDamageable.cs
+++ Wizards vs Drones/Assets/Scripts/Interfaces/IWVDDamageable.cs
@@ -4,7 +4,7 @@ public interface IWVDDamageable
 {
     void ResolveAttack(int damage, WVDAttackEffects effects);
     void TakeDamage(int damage); // todo may need additional inputs, which could put in a custom struct, like stun, DOT etc.
-    void DestroyFullyDamaged();
+    //void DestroyFullyDamaged(); todo probably won't need in this interface
 
     Transform GetTransform();
 
