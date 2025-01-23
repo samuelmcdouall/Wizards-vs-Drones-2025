47efd439f7b88a68ae7a70212aee83551d309f65
samuelmcdouall <samuelmcdouall@gmail.com>
2025-01-23 16:34:23 +0000

    Boss idle/flying in dungeon states done

--- Wizards vs Drones/Assets/Scripts/WVDBoss.cs
+++ Wizards vs Drones/Assets/Scripts/WVDBoss.cs
@@ -6,14 +6,71 @@ using UnityEngine.AI;
 
 public class WVDBoss : WVDBaseEntity
 {
+    [Header("General - Boss")]
     BossState _currentBossState;
-    NavMeshAgent BossNMA;
+
+    [Header("Movement - Boss")]
+    Vector3 _movementVector;
+
+    [Header("Dungeon Idle - Boss")]
+    [SerializeField]
+    List<Transform> _dungeonIdleWayPoints;
+    Transform _chosenWayPoint;
+    [SerializeField]
+    float _wayPointThreshold;
+    [SerializeField]
+    float _minDungeonIdleTime;
+    [SerializeField]
+    float _maxDungeonIdleTime;
+    float _dungeonIdleTimer;
+
+
 
     public override void Start()
     {
         base.Start();
         _currentBossState = BossState.DungeonIdle;
-        BossNMA = GetComponent<NavMeshAgent>();
+        _dungeonIdleTimer = Random.Range(_minDungeonIdleTime, _maxDungeonIdleTime);
+        _chosenWayPoint = _dungeonIdleWayPoints[Random.Range(0, _dungeonIdleWayPoints.Count)];
+    }
+
+    public override void Update()
+    {
+        //base.Update(); todo probably don't need this as boss cannot be slowed/stunned or invul
+        if (_currentBossState == BossState.DungeonIdle)
+        {
+            if (_dungeonIdleTimer < 0.0f)
+            {
+                _dungeonIdleTimer = Random.Range(_minDungeonIdleTime, _maxDungeonIdleTime);
+                Transform initialChosenWayPoint = _dungeonIdleWayPoints[Random.Range(0, _dungeonIdleWayPoints.Count)];
+                while (initialChosenWayPoint == _chosenWayPoint)
+                {
+                    initialChosenWayPoint = _dungeonIdleWayPoints[Random.Range(0, _dungeonIdleWayPoints.Count)]; // making sure we don't choose the same one
+                }
+                _chosenWayPoint = initialChosenWayPoint;
+                _movementVector = (_chosenWayPoint.position - transform.position).normalized;
+                transform.LookAt(_chosenWayPoint);
+                SwitchToAnimation(WVDAnimationStrings.BossFlyingAnimation);
+                _currentBossState = BossState.DungeonFlying;
+            }
+            else
+            {
+                _dungeonIdleTimer -= Time.deltaTime;
+            }
+        }
+        else if (_currentBossState == BossState.DungeonFlying)
+        {
+            if (Vector3.Distance(transform.position, _chosenWayPoint.position) < _wayPointThreshold)
+            {
+                SwitchToAnimation(WVDAnimationStrings.BossIdleAnimation);
+                _currentBossState = BossState.DungeonIdle;
+            }
+            else
+            {
+                transform.position += _movementVector * MaxNormalSpeed * Time.deltaTime;
+            }
+        }
+
     }
 
 
