cf3877b268e9748e012ec9e0bc04d4d4282e3ccb
samuelmcdouall <samuelmcdouall@gmail.com>
2025-01-24 21:35:06 +0000

    Changed the 3 shot arc to 15 degrees each way instead of 30

--- Wizards vs Drones/Assets/Scripts/WVDPlayerInputs.cs
+++ Wizards vs Drones/Assets/Scripts/WVDPlayerInputs.cs
@@ -132,13 +132,13 @@ public class WVDPlayerInputs : MonoBehaviour
 
                 if (_playerScript.PurchasedUpgrades.ShootThreeArc)
                 {
-                    WVDPlayerProjectile magicMissileLeft = Instantiate(_magicMissilePrefab, _attackFirePoint.position, Quaternion.Euler(0.0f, -30.0f, 0.0f) * _playerScript.GetModelTransform().rotation).GetComponent<WVDPlayerProjectile>();
-                    magicMissileLeft.SetProjectileDirection(Quaternion.Euler(0.0f,-30.0f,0.0f) * direction);
+                    WVDPlayerProjectile magicMissileLeft = Instantiate(_magicMissilePrefab, _attackFirePoint.position, Quaternion.Euler(0.0f, -15.0f, 0.0f) * _playerScript.GetModelTransform().rotation).GetComponent<WVDPlayerProjectile>();
+                    magicMissileLeft.SetProjectileDirection(Quaternion.Euler(0.0f,-15.0f,0.0f) * direction);
                     magicMissileLeft.SetProjectileEffects(currentEffects);
                     magicMissileLeft.Damage += bonusDamage;
                     magicMissileLeft.PlayerScript = _playerScript;
-                    WVDPlayerProjectile magicMissileRight = Instantiate(_magicMissilePrefab, _attackFirePoint.position, Quaternion.Euler(0.0f, 30.0f, 0.0f) * _playerScript.GetModelTransform().rotation).GetComponent<WVDPlayerProjectile>();
-                    magicMissileRight.SetProjectileDirection(Quaternion.Euler(0.0f, 30.0f, 0.0f) * direction);
+                    WVDPlayerProjectile magicMissileRight = Instantiate(_magicMissilePrefab, _attackFirePoint.position, Quaternion.Euler(0.0f, 15.0f, 0.0f) * _playerScript.GetModelTransform().rotation).GetComponent<WVDPlayerProjectile>();
+                    magicMissileRight.SetProjectileDirection(Quaternion.Euler(0.0f, 15.0f, 0.0f) * direction);
                     magicMissileRight.SetProjectileEffects(currentEffects);
                     magicMissileRight.Damage += bonusDamage;
                     magicMissileRight.PlayerScript = _playerScript;
