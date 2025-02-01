be391f6b1945d01f6618484aa0f5256e70e86984
samuelmcdouall <samuelmcdouall@gmail.com>
2025-02-01 17:15:09 +0000

    Update IWVDDamageable.cs

--- Wizards vs Drones/Assets/Scripts/Interfaces/IWVDDamageable.cs
+++ Wizards vs Drones/Assets/Scripts/Interfaces/IWVDDamageable.cs
@@ -4,7 +4,6 @@ public interface IWVDDamageable
 {
     void ResolveAttack(int damage, WVDAttackEffects effects);
     void TakeDamage(int damage, bool playDamageSFX); // the playDamageSFX is really just for not playing it when the tome is picked up, as that will be very loud
-    //void DestroyFullyDamaged(); todo probably won't need in this interface
 
     Transform GetTransform();
 
